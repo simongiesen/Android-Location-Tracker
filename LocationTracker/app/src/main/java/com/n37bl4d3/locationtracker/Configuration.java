e346d155732e5da1dc5a749c6a1dc8081dd4aef2
Viliyan Vasilev <n37bl4d3@gmail.com>
2017-05-04 14:46:57 +0300

    Update Configuration.java

--- LocationTracker/app/src/main/java/com/n37bl4d3/locationtracker/Configuration.java
+++ LocationTracker/app/src/main/java/com/n37bl4d3/locationtracker/Configuration.java
@@ -4,11 +4,11 @@ import java.io.File;
 
 public class Configuration {
 
-    public static File sFilesDir;
-    public static String sFilesDirPath;
+    public static File sFilesDirMemory;
+    public static String sFilesDirPathMemory;
 
-    public static File sCacheDir;
-    public static String sCacheDirPath;
+    public static File sCacheDirMemory;
+    public static String sCacheDirPathMemory;
 
     public static String sApplicationName;
 
@@ -17,18 +17,23 @@ public class Configuration {
 
     public static final boolean LOGGING_VERBOSE = true;
     public static final boolean LOGGING_VERBOSE_SAVE = true;
+    public static final String LOGGING_VERBOSE_SAVE_FILE_NAME = "verboseLog.log";
 
     public static final boolean LOGGING_DEBUG = true;
     public static final boolean LOGGING_DEBUG_SAVE = true;
+    public static final String LOGGING_DEBUG_SAVE_FILE_NAME = "debugLog.log";
 
     public static final boolean LOGGING_INFO = true;
     public static final boolean LOGGING_INFO_SAVE = true;
+    public static final String LOGGING_INFO_SAVE_FILE_NAME = "infoLog.log";
 
     public static final boolean LOGGING_WARN = true;
     public static final boolean LOGGING_WARN_SAVE = true;
+    public static final String LOGGING_WARN_SAVE_FILE_NAME = "warnLog.log";
 
     public static final boolean LOGGING_ERROR = true;
     public static final boolean LOGGING_ERROR_SAVE = true;
+    public static final String LOGGING_ERROR_SAVE_FILE_NAME = "errorLog.log";
 
     public static boolean sIsFeatureLocationAvailable;
     public static boolean sIsFeatureLocationNetworkAvailable;
